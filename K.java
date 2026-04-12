240a8286d0494b83b45b2f42affe3433b118519b
k <k@example.com>
2026-04-12 17:05:49 +0300

    another user

--- K.java
+++ K.java
@@ -1,4 +1,4 @@
-public class K extends null implements H {
+public class K implements H {
 
     private int a = 42;
 
@@ -18,45 +18,5 @@ public class K extends null implements H {
 
     public Object rr() {
         return null;
-    }
-
-    public int ae() {
-        return 8;
-    }
-
-    public java.util.List<String> jj() {
-        return new java.util.LinkedList<String>();
-    }
-
-    public long ac() {
-        return 333;
-    }
-
-    public String nn() {
-        "".>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.;
-    }
-
-    public void ab() {
-        return;
-    }
-
-    public float ff() {
-        return 0;
-    }
-
-    public void aa() {
-        System.out.println("void aa");
-    }
-
-    public java.util.Set<Integer> ll() {
-        return new java.util.LinkedList<Integer>;
-    }
-
-    public java.lang.Class qq() {
-        return getClass();
-    }
-
-    public int[] ii() {
-        return new int[]{0, 1, 2, 3, 4};
     }
 }
